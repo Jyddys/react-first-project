6717881a6e39cd3beba50bf0a5df70913ae4a7f3
Jyddys <justus.sydanmaa@hotmail.com>
2023-01-10 14:03:17 +0200

    muutettiin expenses haku map arrayksi

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,55 +1,26 @@
 import ExpenseItem from "./ExpenseItem"
 import "./Expenses.css"
+import React from "react"
 
 
 function Expenses(props) {
 
+    const items = props.expenses.map((expense, index) => (
+        <ExpenseItem
+                key={index}
+                title = {expense.title}
+                amount = {expense.amount}
+                date = {expense.date}
+        />
+    ))
+        
+
     return (
         <div className="expenses">
-            <ExpenseItem
-                title = {props.expenses[0].title}
-                amount = {props.expenses[0].amount}
-                date = {props.expenses[0].date}
-                />
-                <ExpenseItem
-                title = {props.expenses[1].title}
-                amount = {props.expenses[1].amount}
-                date = {props.expenses[1].date}
-                />
-                <ExpenseItem
-                title = {props.expenses[2].title}
-                amount = {props.expenses[2].amount}
-                date = {props.expenses[2].date}
-                />
-                <ExpenseItem
-                title = {props.expenses[3].title}
-                amount = {props.expenses[3].amount}
-                date = {props.expenses[3].date}
-                />
+            {items}
         </div>
     )
 }
 
 export default Expenses
 
-
-/* <ExpenseItem
-      title = {expenses[0].title}
-      amount = {expenses[0].amount}
-      date = {expenses[0].date}
-      />
-      <ExpenseItem
-      title = {expenses[1].title}
-      amount = {expenses[1].amount}
-      date = {expenses[1].date}
-      />
-      <ExpenseItem
-      title = {expenses[2].title}
-      amount = {expenses[2].amount}
-      date = {expenses[2].date}
-      />
-      <ExpenseItem
-      title = {expenses[3].title}
-      amount = {expenses[3].amount}
-      date = {expenses[3].date}
-      /> */

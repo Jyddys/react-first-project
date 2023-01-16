49ac3b5e80be5f3289de5ef3409cec7d161d96e0
Jyddys <justus.sydanmaa@hotmail.com>
2023-01-16 15:19:43 +0200

    test commit

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import "./ExpenseForm.css"
 
 const ExpenseForm = () => {
-
+    
     const [enteredTitle, setEnteredTitle] = useState('')
     const [enteredAmount, setEnteredAmount] = useState('')
     const [enteredDate, setEnteredDate] = useState('')
